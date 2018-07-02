940bd17aa03225e98bb9f43ade6da21a4e11f8d6
lh <642404044@qq.com>
2018-07-02 23:11:18 +0800

    init entity

---
+++ entity/src/main/java/com/lh/entity/Order.java
@@ -0,0 +1,138 @@
+package com.lh.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 订单
+ */
+@Entity(name = "order_t")
+public class Order {
+
+    @Id
+    private String orderId;
+    //渠道id
+    private String chanId;
+    private String chanUserId;
+    /**
+     * @see com.lh.entity.enums.OrderType
+     */
+    private String orderType;
+    private String productId;
+    private BigDecimal amount;
+    private String outerOrderId;
+    /**
+     * @see com.lh.entity.enums.OrderStatus
+     */
+    private String orderStatus;
+    private String memo;
+    private Date createAt;
+    private Date updateAt;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", chanId='" + chanId + '\'' +
+                ", chanUserId='" + chanUserId + '\'' +
+                ", orderType='" + orderType + '\'' +
+                ", productId='" + productId + '\'' +
+                ", amount=" + amount +
+                ", outerOrderId='" + outerOrderId + '\'' +
+                ", orderStatus='" + orderStatus + '\'' +
+                ", memo='" + memo + '\'' +
+                ", createAt=" + createAt +
+                ", updateAt=" + updateAt +
+                '}';
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getChanId() {
+        return chanId;
+    }
+
+    public void setChanId(String chanId) {
+        this.chanId = chanId;
+    }
+
+    public String getChanUserId() {
+        return chanUserId;
+    }
+
+    public void setChanUserId(String chanUserId) {
+        this.chanUserId = chanUserId;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getOuterOrderId() {
+        return outerOrderId;
+    }
+
+    public void setOuterOrderId(String outerOrderId) {
+        this.outerOrderId = outerOrderId;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
---
+++ entity/src/main/java/com/lh/entity/Product.java
@@ -0,0 +1,155 @@
+package com.lh.entity;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 产品
+ */
+@Entity
+public class Product {
+
+    @Id
+    private String id;
+    private String name;
+    /**
+     * @see com.lh.entity.enums.ProductStatus
+     */
+    private String status;
+    //起投金额
+    private BigDecimal thresholdAmount;
+    //投资步长
+    private BigDecimal stepAmount;
+    //锁定期
+    private Integer lockTerm;
+    //收益率，因为要与其他数相乘，所以使用BigDecimal
+    private BigDecimal rewardRate;
+    private String memo;
+    private Date createAt;
+    private Date updateAt;
+    private String createUser;
+    private String updateUser;
+
+    public static void main(String[] args) {
+        System.out.print(new Product());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", thresholdAmount=" + thresholdAmount +
+                ", stepAmount=" + stepAmount +
+                ", lockTerm=" + lockTerm +
+                ", rewardRate=" + rewardRate +
+                ", memo='" + memo + '\'' +
+                ", createAt=" + createAt +
+                ", updateAt=" + updateAt +
+                ", createUser='" + createUser + '\'' +
+                ", updateUser='" + updateUser + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public BigDecimal getThresholdAmount() {
+        return thresholdAmount;
+    }
+
+    public void setThresholdAmount(BigDecimal thresholdAmount) {
+        this.thresholdAmount = thresholdAmount;
+    }
+
+    public BigDecimal getStepAmount() {
+        return stepAmount;
+    }
+
+    public void setStepAmount(BigDecimal stepAmount) {
+        this.stepAmount = stepAmount;
+    }
+
+    public Integer getLockTerm() {
+        return lockTerm;
+    }
+
+    public void setLockTerm(Integer lockTerm) {
+        this.lockTerm = lockTerm;
+    }
+
+    public BigDecimal getRewardRate() {
+        return rewardRate;
+    }
+
+    public void setRewardRate(BigDecimal rewardRate) {
+        this.rewardRate = rewardRate;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+}
